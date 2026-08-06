a7384e810ac5bb18e9eaf5977a8496b196764613
TuffGenda <tuff.genda@gmail.com>
2026-08-06 09:47:45 -0400

    Initial Commit for Project Files
    All the default Unreal Engine files to start this project on the repo.

---
+++ Source/Carribean_Plunder/Carribean_Plunder.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Carribean_Plunder.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Carribean_Plunder, "Carribean_Plunder" );
